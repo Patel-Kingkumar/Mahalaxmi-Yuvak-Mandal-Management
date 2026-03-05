57fc650af295fdc5a2d56c0e6f688fec36d1cb7c
Patel King <kingpatel0409@gmail.com>
2026-03-06 00:53:17 +0530

    012: Menu and sub-menu open and close resolve

--- mahalaxmi_yuvak_mandal.client/src/app/components/layout/layout.component.ts
+++ mahalaxmi_yuvak_mandal.client/src/app/components/layout/layout.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare var $: any;
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit, AfterViewInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+
+        setTimeout(() => {
+          $('[data-widget="treeview"]').Treeview('init');
+        });
+
+      }
+
+    });
+
+  }
+
+  ngAfterViewInit(): void {
+
+    $('[data-widget="treeview"]').Treeview('init');
+
+  }
 
 }
